353cdff6cb28e3f4e2a70c9e515a4b22e23088ac
IrinaGladysheva <irina.gladyscheva@code.berlin>
2023-04-11 15:51:20 +0200

    feat: add description field

--- app/orders/models.py
+++ app/orders/models.py
@@ -22,4 +22,5 @@ class Works(db.Model, CRUDMixin):
   id = db.Column(db.Integer, primary_key=True)
   slug = db.Column(db.String(80), unique=True)
   name = db.Column(db.String(80))
+  description = db.Column(db.String(200))
   price = db.Column(db.Numeric(10, 2))
