7edebe1002278deb81872e4896938a771b42f199
IrinaGladysheva <irina.gladyscheva@code.berlin>
2023-04-18 15:11:09 +0200

    data base edit

--- app/orders/routes.py
+++ app/orders/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app.extensions.database import db
 from app.orders.models import Order, Address, Works
 
@@ -10,9 +10,17 @@ from app.orders import models
 def homework():
   return render_template('homework.html')
 
-@orders_blueprint.route('/about')
+@orders_blueprint.route('/about', methods=['GET', 'POST'])
 def about():
 #  works = ["Screaming Face", "Russian sadness", "Trapped inside my mind", "Loving"]
+  if request.method == 'POST':
+    name = request.form['title']
+    description = request.form['description']
+    price = request.form['price']
+    new_work = Works(slug='about', name=name, description=description, price=price)
+    new_work.save()
+    #db.session.add(works)
+    #db.session.commit()
   works = Works.query.all()
   return render_template('About.html', works=works)
 
